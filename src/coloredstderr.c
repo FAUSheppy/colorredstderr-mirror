b140cc0a823c533fdc1aefe46281d042545036b1
Simon Ruderich <simon@ruderich.org>
2013-06-05 18:11:33 +0200

    coloredstderr.c: Use #include <config.h>.
    This ensures that config.h from the build directory is preferred over a
    possible file of the same name in the source directory.

--- src/coloredstderr.c
+++ src/coloredstderr.c
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "config.h"
+#include <config.h>
 
 /* Must be loaded before the following headers. */
 #include "ldpreload.h"
