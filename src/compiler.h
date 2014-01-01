f54cd101d750cb31f54081a41d6cb880a9199dba
Simon Ruderich <simon@ruderich.org>
2014-01-01 16:41:01 +0100

    Update copyright year.

--- src/compiler.h
+++ src/compiler.h
@@ -1,7 +1,7 @@
 /*
  * Compiler specific macros.
  *
- * Copyright (C) 2013  Simon Ruderich
+ * Copyright (C) 2013-2014  Simon Ruderich
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
